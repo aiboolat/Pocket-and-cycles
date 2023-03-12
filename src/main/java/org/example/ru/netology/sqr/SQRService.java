17541620a94cd62aef397f58ff5f349b804c62a1
Aybulat <ayba.irgalin@gmail.com>
2023-03-12 22:54:04 +0600

    Ispravil

--- src/main/java/org/example/ru/netology/sqr/SQRService.java
+++ src/main/java/org/example/ru/netology/sqr/SQRService.java
@@ -4,7 +4,7 @@ public class SQRService {
 
     public int getcount(int minValue, int maxValue) {
         int count = 0;
-        for (int i = 0; i <= maxValue; i++) {
+        for (int i = 10; i <= 99; i++) {
             int i2 = i * i;
             if (i2 >= minValue && i2 <= maxValue) {
                 count++;
